82914229f87444297213ed9d53a0a5a3ffa1dcdc
Pedro Minga <Administrador@BT312599.uio.bpichincha.com>
2022-06-16 10:37:15 -0500

    Carga inicial

---
+++ ejemplo_prueba/src/main/java/com/pichincha/prueba/demo/entity/ProductOrder.java
@@ -0,0 +1,39 @@
+package com.pichincha.prueba.demo.entity;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductOrder {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "order_id")
+	private Long id;
+	private Date dateOrdered;
+	@OneToMany(mappedBy = "orderOwner", fetch = FetchType.LAZY,cascade = CascadeType.ALL)
+	private List<DetailOrder> details;
+	
+	@ManyToOne(fetch = FetchType.LAZY, optional = true)
+	@JoinColumn(name = "user_id", nullable = true)
+	private UserStore userOwner;
+}
---
+++ ejemplo_prueba/src/main/java/com/pichincha/prueba/demo/service/StoreService.java
@@ -0,0 +1,8 @@
+package com.pichincha.prueba.demo.service;
+
+import com.pichincha.prueba.demo.dto.StoreDto;
+
+public interface StoreService {
+
+	boolean saveStore(StoreDto storeDto);
+}
